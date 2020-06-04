4a3385f7ab494255c46a43c5b8011235d392cbb5
bubusuke <ysk.tkch@gmail.com>
2020-06-04 10:47:22 +0900

    add mod calculations.

--- cpp/utils/calc.cpp
+++ cpp/utils/calc.cpp
@@ -7,6 +7,51 @@ int sample(){
     return pow(2, 3); //2^3
 }
 
+template <typename T>
+T modMinus(T a, T b, T mod) {
+  return (a>b)? (a-b)%mod: modMinus(mod+a, b, mod);
+}
+
+template <typename T>
+T modPow(T base, T e, T mod) {
+  if(e==0) return 1;
+  if(e==1) return base%mod;
+  if(e%(T)2==1) return (base * modPow(base, e-(T)1, mod)) %mod;
+  
+  T tmp = modPow(base, e/(T)2, mod);
+  return (tmp * tmp) % mod;
+}
+
+template <typename T>
+T primeModDiv(T num, T den, T primeMod){
+  return ( num*modPow(den, primeMod-(T)2, primeMod) )%primeMod;
+}
+
+template <typename T>
+T modComb(T n, T k, T mod) {
+  T num = (T)1; //numerator
+  T den = (T)1; //denominator
+  if (k > n-k) k = n-k;
+  for (T i = 1; i <= k; i++) {
+    num *= (n-i+(T)1);
+    num %= mod;
+    den *= i;
+    den %= mod;
+  }
+  return primeModDiv(num, den, mod);
+}
+
+template <typename T>
+T Combination(T n, T k) {
+  T val = 1;
+  if (k > n-k) k = n-k;
+  for (T i = 1; i <= k; i++) {
+    val *= (n-i+1);
+    val /= i;
+  }
+  return val;
+}
+
 //  long mod =  1e9+7;   10^9+7を意味する
 
 
